7b7a30afef556d022f0e026ff29695377e64d926
FedrianzD <fedrianzd@gmail.com>
2024-06-08 11:09:32 +0700

    docker mysql

--- src/frontend/obj/Debug/net8.0-windows/frontend.AssemblyInfo.cs
+++ src/frontend/obj/Debug/net8.0-windows/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ee1efc93b38b680e36d745159b8035185243a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7643ed60474b3f2877b3fb6d242efc1f16bcbe84")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
